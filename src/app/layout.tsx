97de12ae8f926917adcb87b2cc63c582a53d208a
Sujal-Kr <sunnykumarcms45@gmail.com>
2024-07-21 20:34:24 +0530

    Conditional navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 
+
 const roboto = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,30 +1,35 @@
-import React from 'react'
+"use client";
+import React from 'react';
 import { PiLinkedinLogoThin } from "react-icons/pi";
 import { PiChatCenteredTextLight } from "react-icons/pi";
-import {CiSearch} from "react-icons/ci";
+import { CiSearch } from "react-icons/ci";
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 const Navbar = () => {
+  const pathname = usePathname();
+  
+  if (["/login", "/signup"].includes(pathname)) return null;
 
   return (
     <nav className='flex justify-between items-end p-2'>
-      <div className="nav-logo flex items-end text-3xl ">
+      <div className="nav-logo flex items-end text-3xl">
         <PiLinkedinLogoThin className='text-4xl' />
         <span className='text-sm hidden sm:block'>ScholarHire</span>
       </div>
-      <div className='right-cont  flex items-end gap-5 w-full max-w-md'>
-        <div className="relative w-full ">
-            <CiSearch className='absolute  text-3xl right-2 top-1'/>
-            <input className=' rounded-lg text-xs border-black border-2 outline-none px-3 py-2 sm:p-3 min-w-full ' type="text" name="" id="" />
+      <div className='right-cont flex items-end gap-5 w-full max-w-md'>
+        <div className="relative w-full">
+          <CiSearch className='absolute text-3xl right-2 top-1' />
+          <input className='rounded-lg text-xs border-black border-2 outline-none px-3 py-2 sm:p-3 min-w-full' type="text" name="" id="" />
         </div>
-       <div className="action-btn text-3xl">
-        <Link href={'/message'}>
+        <div className="action-btn text-3xl">
+          <Link href={'/message'}>
             <PiChatCenteredTextLight />
-        </Link>
-       </div>
+          </Link>
+        </div>
       </div>
-      
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
