8155ec3a3b3ae8fde2044220d57bf540fdf6b745
Sujal-Kr <sunnykumarcms45@gmail.com>
2024-07-23 00:03:45 +0530

    schema updated

--- src/schema/LoginSchema.ts
+++ src/schema/LoginSchema.ts
@@ -3,4 +3,4 @@ import { z } from 'zod'
 export const LoginSchema = z.object({
     email: z.string().email({ message: "Please enter a valid email address" }),
     password: z.string().min(8, { message: "Must be 8 or more characters long" }),
-})
+}).required()
--- src/schema/SignUpSchema.ts
+++ src/schema/SignUpSchema.ts
@@ -8,4 +8,4 @@ export const SignUpSchema = z.object({
       .regex(/^[A-Za-z\s]+$/, { message: "Special Characters not allowed" }),
     email: z.string().email({ message: "Please enter a valid email address" }),
     password: z.string().min(8, { message: "Must be 8 or more characters long" }),
-});
+}).required();
