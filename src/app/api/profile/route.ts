5f83225246dddd14d46d713b65c89748c9da6bec
Shivam_Sharma <shivam.sharma2023@vitstudent.ac.in>
2024-08-28 12:22:54 +0530

    chore: Update profile route to fetch user details with aggregate query

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -2,7 +2,8 @@ import { User } from '@/model/user.model'
 import * as JWT from 'jsonwebtoken'
 import { NextRequest, NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
-
+import { Types } from 'mongoose'
+import Profile from '@/model/profile.model'
 
 export async function GET(req: NextRequest) {
     try {
@@ -10,19 +11,46 @@ export async function GET(req: NextRequest) {
 
         const token = cookieStore.get('token')?.value
 
-        if(!token) return NextResponse.json({ message: 'Token not found' }, { status: 400 })
+        if (!token) return NextResponse.json({ message: 'Token not found' }, { status: 400 })
 
         const payload = JWT.verify(token, process.env.JWT_SECRET_KEY!) as JWT.JwtPayload
-        
-        const userId = payload.id as string
 
-        const user = await User.findById(userId).select('-password -verifyCode -verifyCodeExpiryDate')
+        const userId = new Types.ObjectId(payload.id as string)
 
+        const user = await Profile.aggregate([
+            {
+                $match:{
+                    userId
+                }
+            },
+            {
+                $lookup:{
+                    from: 'users',
+                    localField: 'userId',
+                    foreignField: '_id',
+                    as: 'user'
+                }
+            },
+            {
+                $unwind: '$user'
+            },
+            {
+                $project:{
+                    user:{
+                        verifyCode: 0,
+                        password: 0,
+                        verifyCodeExpiryDate:0
+                    }
+                }
+            }
+        ])
+
+        console.log(user, "[User Details from the aggregate query]")
         if (!user) {
             return NextResponse.json({ message: 'User not found' }, { status: 404 })
         }
         // const userId = payload.id as string
-        return NextResponse.json({ 
+        return NextResponse.json({
             message: 'Profile fetched',
             user
         }, { status: 200 })
